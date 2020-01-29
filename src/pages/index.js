4ff4eddb0272b1403deba0a2f56a928d53ff3e2c
Francis Albores <mrthemetribe@gmail.com>
2020-01-29 14:45:08 +0800

    feat : Box with front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,55 @@
 import React from "react"
+import avatar from "../images/fma.jpg"
+import "./../styles/style.scss"
 
-export default () => <div>Hello world!</div>
+class App extends React.Component{
+    constructor(props) {super(props); }
+
+    render() {
+        return (
+            <div>
+                <div className="box-container">
+                    <div className="box" id="box">
+                    <div id="top">
+
+
+                    </div>
+                    <div id="front">
+
+                        <div className="face-container">
+                        <div style={{flex:1}}>
+                            <h2>Francis Albores</h2>
+                            <sub>Web/Mobile Developer</sub>
+                            <hr />
+                            <h3 className="rotatingSlogan">60% Frontend</h3 >
+                        </div>
+                        <img src={avatar}  className="avatar" />
+                        </div>
+
+                    </div>
+                    <div id="bottom">
+
+
+                    </div>
+                    <div id="back">
+
+
+                    </div>
+                    <div id="side-left">
+
+
+                    </div>
+                    <div id="side-right">
+
+
+                    </div>
+                    </div>
+                </div>
+                <div id="screen">
+
+                </div>
+            </div>
+        )
+    }
+}
+export default App
